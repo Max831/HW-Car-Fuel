3c9169e5ff9c01c3c29628509c617ea1bf34bf8d
Maxim <vaslyaev.m.m@effective-group.ru>
2019-11-18 01:52:56 +0300

    fixed service

--- src/main/java/ru/rosbank/javaschool/service/CarService.java
+++ src/main/java/ru/rosbank/javaschool/service/CarService.java
@@ -3,18 +3,14 @@ package ru.rosbank.javaschool.service;
 import ru.rosbank.javaschool.repository.Car;
 
 public class CarService {
+    private final static int zeroValue = 0;
 
     public void distance(Car car) {
-        if (car.getChargeFuel() > 0 && car.getVolumeFuel() >= 0) {
-            if (car.getVolumeFuel() == 0){
-                car.setDistance(0);
-                System.out.println("Empty Fuel. Distance = 0");
-            } else {
+        if (car.getChargeFuel() > zeroValue && car.getVolumeFuel() > zeroValue) {
                 car.setDistance((int) (car.getVolumeFuel() / car.getChargeFuel() *100));
                 System.out.println(car.getDistance());
-            }
         } else {
-            car.setDistance(0);
+            car.setDistance(zeroValue);
             System.out.println("Incorrect parameters");
         }
     }
