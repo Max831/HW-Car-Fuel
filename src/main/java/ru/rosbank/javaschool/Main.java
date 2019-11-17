73be0f0c07c7cfff57db2c0054ba19668119743e
Maxim <vaslyaev.m.m@effective-group.ru>
2019-11-17 22:09:49 +0300

    Car_Fuel

---
+++ src/main/java/ru/rosbank/javaschool/Main.java
@@ -0,0 +1,16 @@
+package ru.rosbank.javaschool;
+
+import ru.rosbank.javaschool.repository.Car;
+import ru.rosbank.javaschool.service.CarService;
+
+public class Main {
+    public static void main(String[] args) {
+        CarService carService = new CarService();
+        Car carNumber1 = new Car(4.6, 45);
+        carService.distance(carNumber1);
+        Car carNumber2 = new Car(7.7, 40);
+        carService.distance(carNumber2);
+        Car carNumber3 = new Car(1.1, 5);
+        carService.distance(carNumber3);
+    }
+}
---
+++ src/test/java/ru/rosbank/javaschool/service/CarServiceTest.java
@@ -0,0 +1,16 @@
+package ru.rosbank.javaschool.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarServiceTest {
+
+    @Test
+    void distance() {
+        System.out.println((int) (0.0 / 0.0 *100));
+        System.out.println((int) (0.0 / 2.0 *100));
+        System.out.println((int) (2.0 / 0.0 *100));
+        System.out.println((int) (-1.0 / 0.0 *100));
+    }
+}
