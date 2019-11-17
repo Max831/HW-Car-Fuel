896075d333a0840c537df4d3a320fc972b5c7832
Maxim <vaslyaev.m.m@effective-group.ru>
2019-11-18 01:35:18 +0300

    fixed Test

--- src/main/java/ru/rosbank/javaschool/service/CarService.java
+++ src/main/java/ru/rosbank/javaschool/service/CarService.java
@@ -7,12 +7,14 @@ public class CarService {
     public void distance(Car car) {
         if (car.getChargeFuel() > 0 && car.getVolumeFuel() >= 0) {
             if (car.getVolumeFuel() == 0){
+                car.setDistance(0);
                 System.out.println("Empty Fuel. Distance = 0");
             } else {
                 car.setDistance((int) (car.getVolumeFuel() / car.getChargeFuel() *100));
                 System.out.println(car.getDistance());
             }
         } else {
+            car.setDistance(0);
             System.out.println("Incorrect parameters");
         }
     }
--- src/test/java/ru/rosbank/javaschool/service/CarServiceTest.java
+++ src/test/java/ru/rosbank/javaschool/service/CarServiceTest.java
@@ -1,16 +1,47 @@
 package ru.rosbank.javaschool.service;
 
 import org.junit.jupiter.api.Test;
+import ru.rosbank.javaschool.repository.Car;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarServiceTest {
+    private static Car car1 = new Car(0.0, 100);
+    private static Car car2 = new Car(-1.0, 100);
+    private static Car car3 = new Car(10.0, 0);
+    private static Car car4 = new Car(-1.0, -10);
+    private static Car car5 = new Car(0.0, 0.0);
+    private static Car car6 = new Car(-1.0, 0.0);
+    private static Car car7 = new Car(100.0, 4.6);
+    private static Car car8 = new Car(9.9, 190);
+    private static CarService carService = new CarService();
+
 
     @Test
-    void distance() {
-        System.out.println((int) (0.0 / 0.0 *100));
-        System.out.println((int) (0.0 / 2.0 *100));
-        System.out.println((int) (2.0 / 0.0 *100));
-        System.out.println((int) (-1.0 / 0.0 *100));
+    void zero() {
+        carService.distance(car1);
+        assertEquals(0, car1.getDistance());
+        carService.distance(car3);
+        assertEquals(0, car3.getDistance());
+        carService.distance(car5);
+        assertEquals(0, car5.getDistance());
+    }
+
+    @Test
+    void negativeValue() {
+        carService.distance(car2);
+        assertEquals(0, car2.getDistance());
+        carService.distance(car4);
+        assertEquals(0, car4.getDistance());
+        carService.distance(car6);
+        assertEquals(0, car6.getDistance());
+    }
+
+    @Test
+    void positiveValue() {
+        carService.distance(car7);
+        assertEquals(4, car7.getDistance());
+        carService.distance(car8);
+        assertEquals(1919, car8.getDistance());
     }
 }
