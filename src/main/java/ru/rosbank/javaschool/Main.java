2f4b56c7e225425b21d5da1d379c0fdec876d195
Maxim Vaslyaev <jest1bmc@gmail.com>
2019-11-24 17:12:15 +0300

    fix unused import

--- src/main/java/ru/rosbank/javaschool/Main.java
+++ src/main/java/ru/rosbank/javaschool/Main.java
@@ -1,8 +1,5 @@
 package ru.rosbank.javaschool;
 
-import ru.rosbank.javaschool.model.Car;
-import ru.rosbank.javaschool.service.CarService;
-
 public class Main {
     public static void main(String[] args) {
     }
